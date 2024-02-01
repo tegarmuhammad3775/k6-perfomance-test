d29c4a1a476d877b9a20c9b4d63db7aa686e214c
Tegar Muhammad Soekarno <adl.tegarmuh@xl.co.id>
2024-02-01 16:03:59 +0700

    add load test with target 5 user, and duration 5s

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ import http from "k6/http"
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
+
+export let options = {
+//Load test
+//Targer 5 user, selama 5 detik
+    stages : [
+        {duration: '5s', target : 5},
+    ],
+}
+
 export default function(){
     group("K6 Get Test", ()=>{
         let response1 = http.get('https://test.k6.io');

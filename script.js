2d43e593655eac056914f4f58a2179df3e34c9ec
Tegar Muhammad Soekarno <adl.tegarmuh@xl.co.id>
2024-02-01 15:17:27 +0700

    Add function check

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+import { check } from "k6";
 import http from "k6/http"
 
 export default function(){
     let response1 = http.get('https://test.k6.io');
+    check(response1, {
+        'is status 200' : (r) => r.status == 200
+    });
 
     let url = "https://reqres.in/api/users";
     let body = JSON.stringify(
       {
-        "name": "morpheus",
-        "job": "leader"
+        "name": "morpheus2",
+        "job": "leader2"
       }
     )
     let response2 = http.post(url,body)
     console.log(JSON.stringify(response2.body))
+    check(response2, {
+        'is status 201' : (r) => r.status == 201
+    });
+    
 }
